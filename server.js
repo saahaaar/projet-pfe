27efafa9782d070a161c86836af7d0018fede7f3
saahaaar <sahar.mansouri@esprit.tn>
2021-04-29 01:51:36 +0200

    3 eme commit

---
+++ server.js
@@ -0,0 +1,13 @@
+/*const express = require('express');
+const path = require('path');
+const app = express();
+
+app.get('/getData', (req,res)=>{
+  res.json(
+   "hello world"
+  )
+})
+app.listen(4600, (req,res)=> {
+  console.log('RUNNING');
+});
+*/ 
